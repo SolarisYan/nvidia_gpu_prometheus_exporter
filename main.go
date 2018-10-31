53a9511cb4a0a18ade1cd0db6564419ffb7e33c8
yangang <yangang@raysdata.com>
2018-10-31 23:24:34 +0800

    fix1

--- main.go
+++ main.go
@@ -109,96 +109,96 @@ var (
 // 	c.fanSpeed.Describe(ch)
 // }
 
-func (c *Collector) Collect(ch chan<- prometheus.Metric) {
-	// Only one Collect call in progress at a time.
-	c.Lock()
-	defer c.Unlock()
+// func (c *Collector) Collect(ch chan<- prometheus.Metric) {
+// 	// Only one Collect call in progress at a time.
+// 	c.Lock()
+// 	defer c.Unlock()
 
-	c.usedMemory.Reset()
-	c.totalMemory.Reset()
-	c.dutyCycle.Reset()
-	c.powerUsage.Reset()
-	c.temperature.Reset()
-	c.fanSpeed.Reset()
+// 	c.usedMemory.Reset()
+// 	c.totalMemory.Reset()
+// 	c.dutyCycle.Reset()
+// 	c.powerUsage.Reset()
+// 	c.temperature.Reset()
+// 	c.fanSpeed.Reset()
 
-	numDevices, err := gonvml.DeviceCount()
-	if err != nil {
-		log.Printf("DeviceCount() error: %v", err)
-		return
-	} else {
-		c.numDevices.Set(float64(numDevices))
-		ch <- c.numDevices
-	}
+// 	numDevices, err := gonvml.DeviceCount()
+// 	if err != nil {
+// 		log.Printf("DeviceCount() error: %v", err)
+// 		return
+// 	} else {
+// 		c.numDevices.Set(float64(numDevices))
+// 		ch <- c.numDevices
+// 	}
 
-	for i := 0; i < int(numDevices); i++ {
-		dev, err := gonvml.DeviceHandleByIndex(uint(i))
-		if err != nil {
-			log.Printf("DeviceHandleByIndex(%d) error: %v", i, err)
-			continue
-		}
+// 	for i := 0; i < int(numDevices); i++ {
+// 		dev, err := gonvml.DeviceHandleByIndex(uint(i))
+// 		if err != nil {
+// 			log.Printf("DeviceHandleByIndex(%d) error: %v", i, err)
+// 			continue
+// 		}
 
-		minorNumber, err := dev.MinorNumber()
-		if err != nil {
-			log.Printf("MinorNumber() error: %v", err)
-			continue
-		}
-		minor := strconv.Itoa(int(minorNumber))
+// 		minorNumber, err := dev.MinorNumber()
+// 		if err != nil {
+// 			log.Printf("MinorNumber() error: %v", err)
+// 			continue
+// 		}
+// 		minor := strconv.Itoa(int(minorNumber))
 
-		uuid, err := dev.UUID()
-		if err != nil {
-			log.Printf("UUID() error: %v", err)
-			continue
-		}
+// 		uuid, err := dev.UUID()
+// 		if err != nil {
+// 			log.Printf("UUID() error: %v", err)
+// 			continue
+// 		}
 
-		name, err := dev.Name()
-		if err != nil {
-			log.Printf("Name() error: %v", err)
-			continue
-		}
+// 		name, err := dev.Name()
+// 		if err != nil {
+// 			log.Printf("Name() error: %v", err)
+// 			continue
+// 		}
 
-		totalMemory, usedMemory, err := dev.MemoryInfo()
-		if err != nil {
-			log.Printf("MemoryInfo() error: %v", err)
-		} else {
-			c.usedMemory.WithLabelValues(minor, uuid, name).Set(float64(usedMemory))
-			c.totalMemory.WithLabelValues(minor, uuid, name).Set(float64(totalMemory))
-		}
+// 		totalMemory, usedMemory, err := dev.MemoryInfo()
+// 		if err != nil {
+// 			log.Printf("MemoryInfo() error: %v", err)
+// 		} else {
+// 			c.usedMemory.WithLabelValues(minor, uuid, name).Set(float64(usedMemory))
+// 			c.totalMemory.WithLabelValues(minor, uuid, name).Set(float64(totalMemory))
+// 		}
 
-		dutyCycle, _, err := dev.UtilizationRates()
-		if err != nil {
-			log.Printf("UtilizationRates() error: %v", err)
-		} else {
-			c.dutyCycle.WithLabelValues(minor, uuid, name).Set(float64(dutyCycle))
-		}
+// 		dutyCycle, _, err := dev.UtilizationRates()
+// 		if err != nil {
+// 			log.Printf("UtilizationRates() error: %v", err)
+// 		} else {
+// 			c.dutyCycle.WithLabelValues(minor, uuid, name).Set(float64(dutyCycle))
+// 		}
 
-		powerUsage, err := dev.PowerUsage()
-		if err != nil {
-			log.Printf("PowerUsage() error: %v", err)
-		} else {
-			c.powerUsage.WithLabelValues(minor, uuid, name).Set(float64(powerUsage))
-		}
+// 		powerUsage, err := dev.PowerUsage()
+// 		if err != nil {
+// 			log.Printf("PowerUsage() error: %v", err)
+// 		} else {
+// 			c.powerUsage.WithLabelValues(minor, uuid, name).Set(float64(powerUsage))
+// 		}
 
-		temperature, err := dev.Temperature()
-		if err != nil {
-			log.Printf("Temperature() error: %v", err)
-		} else {
-			c.temperature.WithLabelValues(minor, uuid, name).Set(float64(temperature))
-		}
+// 		temperature, err := dev.Temperature()
+// 		if err != nil {
+// 			log.Printf("Temperature() error: %v", err)
+// 		} else {
+// 			c.temperature.WithLabelValues(minor, uuid, name).Set(float64(temperature))
+// 		}
 
-		fanSpeed, err := dev.FanSpeed()
-		if err != nil {
-			log.Printf("FanSpeed() error: %v", err)
-		} else {
-			c.fanSpeed.WithLabelValues(minor, uuid, name).Set(float64(fanSpeed))
-		}
-	}
-	c.usedMemory.Collect(ch)
-	c.totalMemory.Collect(ch)
-	c.dutyCycle.Collect(ch)
-	c.powerUsage.Collect(ch)
-	c.temperature.Collect(ch)
-	c.fanSpeed.Collect(ch)
-}
+// 		fanSpeed, err := dev.FanSpeed()
+// 		if err != nil {
+// 			log.Printf("FanSpeed() error: %v", err)
+// 		} else {
+// 			c.fanSpeed.WithLabelValues(minor, uuid, name).Set(float64(fanSpeed))
+// 		}
+// 	}
+// 	c.usedMemory.Collect(ch)
+// 	c.totalMemory.Collect(ch)
+// 	c.dutyCycle.Collect(ch)
+// 	c.powerUsage.Collect(ch)
+// 	c.temperature.Collect(ch)
+// 	c.fanSpeed.Collect(ch)
+// }
 
 type Exporter struct {
 	up                    prometheus.Gauge
