bf96c30a4c5be39a6015900b5a1d950677498dfc
yangang <yangang@raysdata.com>
2018-10-31 23:18:27 +0800

    update

--- main.go
+++ main.go
@@ -18,90 +18,96 @@ const (
 
 var (
 	addr = flag.String("web.listen-address", ":9445", "Address to listen on for web interface and telemetry.")
-
-	labels = []string{"minor_number", "uuid", "name"}
+	// metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+	// labels = []string{"minor_number", "uuid", "name"}
 )
 
-type Collector struct {
-	sync.Mutex
-	numDevices  prometheus.Gauge
-	usedMemory  *prometheus.GaugeVec
-	totalMemory *prometheus.GaugeVec
-	dutyCycle   *prometheus.GaugeVec
-	powerUsage  *prometheus.GaugeVec
-	temperature *prometheus.GaugeVec
-	fanSpeed    *prometheus.GaugeVec
-}
+	// var (
+	// 	listenAddress = flag.String("web.listen-address", ":9401", "Address to listen on for web interface and telemetry.")
+	// 	metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+	// )
+	// flag.Parse()
 
-func NewCollector() *Collector {
-	return &Collector{
-		numDevices: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "num_devices",
-				Help:      "Number of GPU devices",
-			},
-		),
-		usedMemory: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "memory_used_bytes",
-				Help:      "Memory used by the GPU device in bytes",
-			},
-			labels,
-		),
-		totalMemory: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "memory_total_bytes",
-				Help:      "Total memory of the GPU device in bytes",
-			},
-			labels,
-		),
-		dutyCycle: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "duty_cycle",
-				Help:      "Percent of time over the past sample period during which one or more kernels were executing on the GPU device",
-			},
-			labels,
-		),
-		powerUsage: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "power_usage_milliwatts",
-				Help:      "Power usage of the GPU device in milliwatts",
-			},
-			labels,
-		),
-		temperature: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "temperature_celsius",
-				Help:      "Temperature of the GPU device in celsius",
-			},
-			labels,
-		),
-		fanSpeed: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "fanspeed_percent",
-				Help:      "Fanspeed of the GPU device as a percent of its maximum",
-			},
-			labels,
-		),
-	}
-}
+// type Collector struct {
+// 	sync.Mutex
+// 	numDevices  prometheus.Gauge
+// 	usedMemory  *prometheus.GaugeVec
+// 	totalMemory *prometheus.GaugeVec
+// 	dutyCycle   *prometheus.GaugeVec
+// 	powerUsage  *prometheus.GaugeVec
+// 	temperature *prometheus.GaugeVec
+// 	fanSpeed    *prometheus.GaugeVec
+// }
 
-func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.numDevices.Desc()
-	c.usedMemory.Describe(ch)
-	c.totalMemory.Describe(ch)
-	c.dutyCycle.Describe(ch)
-	c.powerUsage.Describe(ch)
-	c.temperature.Describe(ch)
-	c.fanSpeed.Describe(ch)
-}
+// func NewCollector() *Collector {
+// 	return &Collector{
+// 		numDevices: prometheus.NewGauge(
+// 			prometheus.GaugeOpts{
+// 				Namespace: namespace,
+// 				Name:      "num_devices",
+// 				Help:      "Number of GPU devices",
+// 			},
+// 		),
+// 		usedMemory: prometheus.NewGaugeVec(
+// 			prometheus.GaugeOpts{
+// 				Namespace: namespace,
+// 				Name:      "memory_used_bytes",
+// 				Help:      "Memory used by the GPU device in bytes",
+// 			},
+// 			labels,
+// 		),
+// 		totalMemory: prometheus.NewGaugeVec(
+// 			prometheus.GaugeOpts{
+// 				Namespace: namespace,
+// 				Name:      "memory_total_bytes",
+// 				Help:      "Total memory of the GPU device in bytes",
+// 			},
+// 			labels,
+// 		),
+// 		dutyCycle: prometheus.NewGaugeVec(
+// 			prometheus.GaugeOpts{
+// 				Namespace: namespace,
+// 				Name:      "duty_cycle",
+// 				Help:      "Percent of time over the past sample period during which one or more kernels were executing on the GPU device",
+// 			},
+// 			labels,
+// 		),
+// 		powerUsage: prometheus.NewGaugeVec(
+// 			prometheus.GaugeOpts{
+// 				Namespace: namespace,
+// 				Name:      "power_usage_milliwatts",
+// 				Help:      "Power usage of the GPU device in milliwatts",
+// 			},
+// 			labels,
+// 		),
+// 		temperature: prometheus.NewGaugeVec(
+// 			prometheus.GaugeOpts{
+// 				Namespace: namespace,
+// 				Name:      "temperature_celsius",
+// 				Help:      "Temperature of the GPU device in celsius",
+// 			},
+// 			labels,
+// 		),
+// 		fanSpeed: prometheus.NewGaugeVec(
+// 			prometheus.GaugeOpts{
+// 				Namespace: namespace,
+// 				Name:      "fanspeed_percent",
+// 				Help:      "Fanspeed of the GPU device as a percent of its maximum",
+// 			},
+// 			labels,
+// 		),
+// 	}
+// }
+
+// func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
+// 	ch <- c.numDevices.Desc()
+// 	c.usedMemory.Describe(ch)
+// 	c.totalMemory.Describe(ch)
+// 	c.dutyCycle.Describe(ch)
+// 	c.powerUsage.Describe(ch)
+// 	c.temperature.Describe(ch)
+// 	c.fanSpeed.Describe(ch)
+// }
 
 func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 	// Only one Collect call in progress at a time.
@@ -194,13 +200,194 @@ func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 	c.fanSpeed.Collect(ch)
 }
 
+type Exporter struct {
+	up                    prometheus.Gauge
+	info                  *prometheus.GaugeVec
+	deviceCount           prometheus.Gauge
+	temperatures          *prometheus.GaugeVec
+	deviceInfo            *prometheus.GaugeVec
+	powerUsage            *prometheus.GaugeVec
+	powerUsageAverage     *prometheus.GaugeVec
+	fanSpeed              *prometheus.GaugeVec
+	memoryTotal           *prometheus.GaugeVec
+	memoryUsed            *prometheus.GaugeVec
+	utilizationMemory     *prometheus.GaugeVec
+	utilizationGPU        *prometheus.GaugeVec
+	utilizationGPUAverage *prometheus.GaugeVec
+}
+
+func NewExporter() *Exporter {
+	return &Exporter{
+		up: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "up",
+				Help:      "NVML Metric Collection Operational",
+			},
+		),
+		info: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "driver_info",
+				Help:      "NVML Info",
+			},
+			[]string{"version"},
+		),
+		deviceCount: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "device_count",
+				Help:      "Count of found nvidia devices",
+			},
+		),
+		deviceInfo: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "info",
+				Help:      "Info as reported by the device",
+			},
+			[]string{"index", "minor", "uuid", "name"},
+		),
+		temperatures: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "temperatures",
+				Help:      "Temperature as reported by the device",
+			},
+			[]string{"minor"},
+		),
+		powerUsage: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "power_usage",
+				Help:      "Power usage as reported by the device",
+			},
+			[]string{"minor"},
+		),
+		powerUsageAverage: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "power_usage_average",
+				Help:      "Power usage as reported by the device averaged over 10s",
+			},
+			[]string{"minor"},
+		),
+		fanSpeed: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "fanspeed",
+				Help:      "Fan speed as reported by the device",
+			},
+			[]string{"minor"},
+		),
+		memoryTotal: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "memory_total",
+				Help:      "Total memory as reported by the device",
+			},
+			[]string{"minor"},
+		),
+		memoryUsed: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "memory_used",
+				Help:      "Used memory as reported by the device",
+			},
+			[]string{"minor"},
+		),
+		utilizationMemory: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "utilization_memory",
+				Help:      "Memory Utilization as reported by the device",
+			},
+			[]string{"minor"},
+		),
+		utilizationGPU: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "utilization_gpu",
+				Help:      "GPU utilization as reported by the device",
+			},
+			[]string{"minor"},
+		),
+		utilizationGPUAverage: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "utilization_gpu_average",
+				Help:      "Used memory as reported by the device averraged over 10s",
+			},
+			[]string{"minor"},
+		),
+	}
+}
+
+func (e *Exporter) Collect(metrics chan<- prometheus.Metric) {
+	data, err := collectMetrics()
+	if err != nil {
+		log.Printf("Failed to collect metrics: %s\n", err)
+		e.up.Set(0)
+		e.up.Collect(metrics)
+		return
+	}
+
+	e.up.Set(1)
+	e.info.WithLabelValues(data.Version).Set(1)
+	e.deviceCount.Set(float64(len(data.Devices)))
+
+	for i := 0; i < len(data.Devices); i++ {
+		d := data.Devices[i]
+		e.deviceInfo.WithLabelValues(d.Index, d.MinorNumber, d.Name, d.UUID).Set(1)
+		e.fanSpeed.WithLabelValues(d.MinorNumber).Set(d.FanSpeed)
+		e.memoryTotal.WithLabelValues(d.MinorNumber).Set(d.MemoryTotal)
+		e.memoryUsed.WithLabelValues(d.MinorNumber).Set(d.MemoryUsed)
+		e.powerUsage.WithLabelValues(d.MinorNumber).Set(d.PowerUsage)
+		e.powerUsageAverage.WithLabelValues(d.MinorNumber).Set(d.PowerUsageAverage)
+		e.temperatures.WithLabelValues(d.MinorNumber).Set(d.Temperature)
+		e.utilizationGPU.WithLabelValues(d.MinorNumber).Set(d.UtilizationGPU)
+		e.utilizationGPUAverage.WithLabelValues(d.MinorNumber).Set(d.UtilizationGPUAverage)
+		e.utilizationMemory.WithLabelValues(d.MinorNumber).Set(d.UtilizationMemory)
+	}
+
+	e.deviceCount.Collect(metrics)
+	e.deviceInfo.Collect(metrics)
+	e.fanSpeed.Collect(metrics)
+	e.info.Collect(metrics)
+	e.memoryTotal.Collect(metrics)
+	e.memoryUsed.Collect(metrics)
+	e.powerUsage.Collect(metrics)
+	e.powerUsageAverage.Collect(metrics)
+	e.temperatures.Collect(metrics)
+	e.up.Collect(metrics)
+	e.utilizationGPU.Collect(metrics)
+	e.utilizationGPUAverage.Collect(metrics)
+	e.utilizationMemory.Collect(metrics)
+}
+
+func (e *Exporter) Describe(descs chan<- *prometheus.Desc) {
+	e.deviceCount.Describe(descs)
+	e.deviceInfo.Describe(descs)
+	e.fanSpeed.Describe(descs)
+	e.info.Describe(descs)
+	e.memoryTotal.Describe(descs)
+	e.memoryUsed.Describe(descs)
+	e.powerUsage.Describe(descs)
+	e.powerUsageAverage.Describe(descs)
+	e.temperatures.Describe(descs)
+	e.up.Describe(descs)
+	e.utilizationGPU.Describe(descs)
+	e.utilizationGPUAverage.Describe(descs)
+	e.utilizationMemory.Describe(descs)
+}
+
 func main() {
 	flag.Parse()
 
-	if err := gonvml.Initialize(); err != nil {
-		log.Fatalf("Couldn't initialize gonvml: %v. Make sure NVML is in the shared library search path.", err)
-	}
-	defer gonvml.Shutdown()
+	// if err := gonvml.Initialize(); err != nil {
+	// 	log.Fatalf("Couldn't initialize gonvml: %v. Make sure NVML is in the shared library search path.", err)
+	// }
+	// defer gonvml.Shutdown()
 
 	if driverVersion, err := gonvml.SystemDriverVersion(); err != nil {
 		log.Printf("SystemDriverVersion() error: %v", err)
@@ -208,7 +395,8 @@ func main() {
 		log.Printf("SystemDriverVersion(): %v", driverVersion)
 	}
 
-	prometheus.MustRegister(NewCollector())
+	// prometheus.MustRegister(NewCollector())
+	prometheus.MustRegister(NewExporter())
 
 	// Serve on all paths under addr
 	log.Fatalf("ListenAndServe error: %v", http.ListenAndServe(*addr, promhttp.Handler()))
